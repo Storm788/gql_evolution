fd9d04c8c3e88a0eed4baa20dcca300c2ca6d776
Hrbolek <profesor@hrbolek.cz>
2025-08-27 20:25:42 +0200

    backup ok

--- src/DBFeeder.py
+++ src/DBFeeder.py
@@ -35,7 +35,6 @@ async def backupDB(asyncSessionMaker, filename="./systemdata.backup.json"):
     import json
     from src.DBDefinitions.BaseModel import IDType
 
-
     dbModels = [
         EventModel, 
         EventInvitationModel,
@@ -48,28 +47,38 @@ async def backupDB(asyncSessionMaker, filename="./systemdata.backup.json"):
             # vsechny radky do dict
             rowsdict = {}
             for row in rows:
-                print(row)
+                # print(row)
                 asdict = dataclasses.asdict(row[0])
                 id = asdict.get("id", None)
                 if id is None: continue
                 rowsdict[id] = asdict
             # vsechny primarní klice do ids
             ids = set(rowsdict.keys())
+            todo = set()
             done = set()
             chunk_id = 0
             while len(done) < len(ids):
                 for row in rowsdict.values():
                     id = row.get("id", None)
                     if id in done: continue
-                    for key, value in asdict.items():
-                        if isinstance(value, IDType):
-                            if value is None: continue
-                            if value in ids:
-                                if value not in done: continue
+                    skip_this_id = False
+                    for key, value in row.items():
+                        if key == "id": continue
+                        # if not isinstance(value, IDType): continue
+                        if value is None: continue
+                        if value not in ids: continue
+                        if value not in done: 
+                            # print(row, key, value)
+                            skip_this_id = True
+                            break
                             # primarni klic je zpracovatelny, nemame zavislost na nezpracovanych klicich
+                    if skip_this_id: continue
                     row["_chunk"] = chunk_id
-                    done.add(id)
-                print(f"Chunk {chunk_id} done {len(done)}/{len(ids)}")
+                    todo.add(id)
+                print(f"{model.__tablename__} chunk {chunk_id} todo/done/all {len(todo)}/{len(done)}/{len(ids)}")
+                if len(todo) == 0: break
+                done = done.union(todo)
+                todo = set()
                 chunk_id += 1
             data.append({
                 model.__tablename__: list(rowsdict.values())
